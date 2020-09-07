ea279600696d49c179ef02749cfbfcaa7bc48aeb
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-08 00:55:13 +0700

    Penambahan index.js serta tugas1

---
+++ index.js
@@ -0,0 +1,15 @@
+var message;
+let msg1 = "Hallo";
+const msg2 = "Coding";
+
+if (true) {
+  msg3 = "Selamat";
+  //   var message = 1;
+  let msg1 = 2;
+  //   console.log("Testing", msg1);
+}
+
+// console.log(message);
+// console.log(msg1);
+console.log(message);
+console.log(msg1);

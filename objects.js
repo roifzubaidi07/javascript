7aa4cd27f0910a5bb3b0010e047c877bf17067bf
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-19 05:03:19 +0700

    Pengerjaan tugas 8

---
+++ objects.js
@@ -0,0 +1,14 @@
+function panggilObjects() {
+  var mobil = {
+    type: "Sedan",
+    harga: 200000000,
+    warna: "putih",
+    tahun: [2001, 2002, 2003, 2004],
+  };
+
+  console.log(mobil);
+  console.log(mobil.type);
+  console.log(mobil.tahun[1]);
+}
+
+panggilObjects();
---
+++ tambah-property.js
@@ -0,0 +1,13 @@
+function panggilObjects() {
+  var mobil = {
+    type: "Sedan",
+    harga: 200000000,
+    warna: "putih",
+  };
+  mobil.tahun = 2018;
+  mobil.harga = 100000000;
+  console.log(mobil);
+  console.log(mobil.harga);
+}
+
+panggilObjects();

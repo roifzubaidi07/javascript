6fc3ee836e948c1522dcf579d92a8fc4991e16e2
M. Ro'if <roifzubaidi5@gmail.com>
2020-09-20 01:55:51 +0700

    Pengerjaan tugas 9

---
+++ ubah-property.js
@@ -0,0 +1,16 @@
+function panggilObjects() {
+  var mobil = {
+    type: "Sedan",
+    harga: 200000000,
+    warna: "putih",
+  };
+  mobil.type = "Kijang";
+  mobil.type = 9500;
+  mobil.type = true;
+  mobil.tahun = 2018;
+  mobil.harga = 100000000;
+  console.log(mobil);
+  console.log(mobil.harga);
+}
+
+panggilObjects();
